0985fb23e66565c898d66ff8ea2faa4314d83ffb
Trang <trang.do@chicagobooth.edu>
2024-05-29 22:36:56 -0400

    updated auth

--- server/server.js
+++ server/server.js
@@ -6,9 +6,15 @@ const userController = require('./controllers/userController.js');
 const cookieController = require('./controllers/cookieController.js');
 const sessionController = require('./controllers/sessionController.js');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src', 'index.html'));
+});
 
 app.get('/action/getUser', userController.getUser, (req, res) => {
     res.json(res.locals.user);
@@ -50,6 +56,10 @@ app.get(
   app.get('/action/logout', sessionController.endSession, (req, res) => {
     res.clearCookie('ssid');
     res.redirect('/');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
   });
 
   module.exports = app;
