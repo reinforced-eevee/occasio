c5e137ee5e4007f211fa0d38a09a0d4b290bab5a
PavelKrapivin <pavelkrapivin.ny@gmail.com>
2024-06-07 14:54:06 -0400

    date conversion works

--- src/components/EventsSidebar.jsx
+++ src/components/EventsSidebar.jsx
@@ -4,41 +4,50 @@ import '../styling/EventsSidebar.css';
 function EventsSidebar({ events, setSelectedEventID }) {
   const [userEvents, setUserEvents] = useState([]);
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/events', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include', // Ensures cookies are sent with the request
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setUserEvents(data);
-          if (data.length > 0) {
-            setSelectedEventID(data[data.length - 1]._id); // Automatically select the last event
-          }
-        } else {
-          throw new Error('Failed to fetch events');
-        }
-      } catch (error) {
-        console.error('Error fetching events:', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchEvents = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:3000/events', {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         credentials: 'include', // Ensures cookies are sent with the request
+  //       });
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         setUserEvents(data);
+  //         if (data.length > 0) {
+  //           setSelectedEventID(data[data.length - 1]._id); // Automatically select the last event
+  //         }
+  //       } else {
+  //         throw new Error('Failed to fetch events');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching events:', error);
+  //     }
+  //   };
 
-    fetchEvents();
-  }, [setSelectedEventID]);
+  //   fetchEvents();
+  // }, [setSelectedEventID]);
+  useEffect(() => {
+    if (events.length > 0) {
+      setSelectedEventID(events[events.length - 1]._id);
+    }
+  }, [events, setSelectedEventID]);
 
   return (
     <div className='events-sidebar'>
       <h3>My Events</h3>
       <div className='events-list'>
-        {userEvents.map((event) => (
+        {events.map((event) => (
           <div
             key={event._id}
-            className='event-icon-box'
+            className={
+              setSelectedEventID === event._id
+                ? 'event-icon-box selected'
+                : 'event-icon-box'
+            }
             onClick={() => setSelectedEventID(event._id)}
           >
             {event.icon} {event.name}
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,52 +1,72 @@
 import React, { useState, useEffect } from 'react';
 
 export default function ShoppingList({ selectedEventID }) {
-    const [shopListDetails, setShopListDetails] = useState([]);
-    const [eventName, setEventName] = useState('');
-    const [eventDate, setEventDate] = useState('');
+  const [shopListDetails, setShopListDetails] = useState([]);
+  const [eventName, setEventName] = useState('');
+  const [eventDate, setEventDate] = useState('');
 
-    const getEvent = () => {
-        fetch(`http://localhost:3000/events/${selectedEventID}`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setShopListDetails(data.shoppingList);
-                setEventName(data.name);
-                setEventDate(data.date);
-            })
-            .catch((err) => console.log('Error retrieving event details: ', err));
-    };
+  const formatDate = (isoDateString) => {
+    const date = new Date(isoDateString);
+    return date.toLocaleDateString('en-CA').replace(/-/g, '/');
+  };
 
-    useEffect(() => {
-        getEvent();
-    }, [selectedEventID]);
-    
-    return (
-        <section className='shoplist-container event-container'>
-            <div className='event-title'>
-                <h1>Shopping List for {eventName} on {eventDate}</h1>
+  const getEvent = () => {
+    fetch(`http://localhost:3000/events/${selectedEventID}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setShopListDetails(data.shoppingList);
+        setEventName(data.name);
+        // setEventDate(data.date);
+        const formattedDate = formatDate(data.date);
+        setEventDate(formattedDate);
+      })
+      .catch((err) => console.log('Error retrieving event details: ', err));
+  };
+
+  useEffect(() => {
+    getEvent();
+  }, [selectedEventID]);
+
+  return (
+    <section className='shoplist-container event-container'>
+      <div className='event-title'>
+        <h1>
+          Shopping List for {eventName} on {eventDate}
+        </h1>
+      </div>
+      <div className='shoppinglist'>
+        {shopListDetails &&
+          shopListDetails.map((item) => (
+            <div className='song-item' key={item._id}>
+              <h3>
+                <a
+                  href={item.item_link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='shopList-titleLink'
+                >
+                  {item.list_item}
+                </a>
+              </h3>
+              <ul>
+                <li>
+                  {' '}
+                  Description:
+                  <ul>
+                    <li>{item.item_description}</li>
+                  </ul>
+                </li>
+                <li>
+                  Estimated Cost:
+                  <ul>
+                    <li>{item.estimated_cost}</li>
+                  </ul>
+                </li>
+              </ul>
             </div>
-            <div className='shoppinglist'>
-                {shopListDetails && shopListDetails.map((item) => (
-                    <div className='song-item' key={item._id}>
-                    <h3><a href={item.item_link} target='_blank' rel='noopener noreferrer' className='shopList-titleLink'>{item.list_item}</a></h3>
-                    <ul>
-                        <li> Description:
-                            <ul>
-                                <li>{item.item_description}</li>
-                            </ul>
-                        </li>
-                        <li>
-                            Estimated Cost:
-                            <ul>
-                                <li>{item.estimated_cost}</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                ))}
-            </div>
-        </section>
-    )
+          ))}
+      </div>
+    </section>
+  );
 }
-
